869611a66a5d6eff9b864be4647b4df12d8eddd7
rplima <raphaeltwins@hotmail.com>
2020-09-29 17:02:08 -0300

    ajustes na pasta e caminhos relativos

--- src/UnitTest/unitTestePlayer.jest.test.js
+++ src/UnitTest/unitTestePlayer.jest.test.js
@@ -1,4 +1,4 @@
-var lib_Player =  require('../src/Model/Player.ts');
+var lib_Player =  require('../Model/Player.ts');
   describe('creat player', () => {
     var player = new lib_Player.Player("jhon");
     test('have 100 lifepoints', () => {
