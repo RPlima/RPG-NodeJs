270ddccba3be45f113df26ef983e7737fbd594e3
rplima <raphaeltwins@hotmail.com>
2020-09-29 18:11:26 -0300

    * Criado os testes para a criação de um player

--- src/Model/Player.ts
+++ src/Model/Player.ts
@@ -1,18 +1,29 @@
+import { isGenericTypeAnnotation } from "@babel/types";
+
 export class Player {
-    private readonly levelInitial: number = 0;
+    private readonly levelInitial: number = 1;
     private readonly minLifePoints: number = 0;
     private readonly lifePointsInitial: number = 100;
+    private readonly experienceInitial: number = 0;
+    private readonly attackInitial: number = 1;
+    private readonly defenseInitial: number = 1;
 
-    constructor(Name: string) 
+    constructor(name: string) 
     {
         this._LifePoints = this.lifePointsInitial;
         this._Level = this.levelInitial;
-        this._Name = Name;
+        this._Name = this.SanitazeNamePlayer(name);
+        this._Experience = this.experienceInitial;
+        this._Attack = this.attackInitial;
+        this._Defense = this.defenseInitial;
     }
 
     private _LifePoints: number;
     private _Level: number;
     private _Name: string;
+    private _Experience: number;
+    private _Attack: number;
+    private _Defense: number;
 
     public get LifePoints(): number 
     {
@@ -28,4 +39,27 @@ export class Player {
     {
         return this._Name;
     }
+
+    public get Experience(): number {
+        return this._Experience;
+    }
+    
+    public get Attack(): number {
+        return this._Attack;
+    }
+
+    public get Defense(): number {
+        return this._Defense;
+    }
+
+    private SanitazeNamePlayer(name:string) : string
+    {
+       if(name.length == 0 || !name.trim())
+       {
+            throw new Error("O nome não pode ser vazio");
+       }
+       return name;
+    }
+
+ 
 }
--- src/UnitTest/unitTestePlayer.jest.test.js
+++ src/UnitTest/unitTestePlayer.jest.test.js
@@ -1,8 +1,29 @@
+const { TestResult } = require('@jest/types');
 var lib_Player =  require('../Model/Player.ts');
   describe('creat player', () => {
-    var player = new lib_Player.Player("jhon");
+    var player = new lib_Player.Player("molina");
     test('have 100 lifepoints', () => {
       expect(player.LifePoints).toBe(100);
+    });
+
+    test('have name not null or empty', () => {
+      expect(player.Name).not.toHaveLength(0);
+    });
+
+    test('have level equals 1', () => {
+      expect(player.Level).toBe(1);
+    });
+
+    test('have experience equals 0', () => {
+      expect(player.Experience).toBe(0);
+    });
+
+    test('have attack equals 1', () => {
+      expect(player.Attack).toBe(1);
+    });
+    
+    test('have defense equals 1', () => {
+      expect(player.Defense).toBe(1);
     });
   });
 
